4bd59e60f941c30f2a5ea8b1653a937048ab3b99
Danielle Gundayao <156253377+goondzy@users.noreply.github.com>
2025-02-24 10:13:50 -0600

    Update test.py

--- test.py
+++ test.py
@@ -6,9 +6,9 @@ greet("Bob")
 
 # Intentional errors below
 
-print(greet("Charlie"  # Missing closing parenthesis
+print(greet("Charlie") # Missing closing parenthesis
 
 x = 10
-y = "20"
+y = 20
 result = x + y  # Trying to add a string and integer
 print(result)
